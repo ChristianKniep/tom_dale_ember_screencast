c2592c097261ad35c5cbd0b5cb93482b8c61a14e
Christian Kniep <christian@qnib.org>
2013-11-10 14:40:51 +0100

    changed app-topic to pins

--- js/app.js
+++ js/app.js
@@ -1,55 +1,61 @@
 App = Ember.Application.create({});
 
+
+
 App.Store = DS.Store.extend({
-  revision: 12,
-  adapter: DS.RESTAdapter.extend({
-    url: 'http://localhost:3000'
+	revision: 12,
+	//adapter: 'DS.FixtureAdapter',
+  	adapter: DS.RESTAdapter.extend({
+    	url: 'http://localhost:8082'
   })
 });
 
 App.Router.map(function() {
-  this.resource('posts', function() {
-    this.resource('post', { path: ':post_id' });
+  this.resource('pins', function() {
+  	this.resource('pin', { path: ':pin_id' })
   });
   this.resource('about');
 });
 
-App.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('posts');
-  }
+
+App.PinsRoute = Ember.Route.extend({
+	model: function() {
+		return App.Pin.find();
+	}
 });
 
-App.PostsRoute = Ember.Route.extend({
-  model: function() {
-    return App.Post.find();
-  }
-});
-
-App.PostController = Ember.ObjectController.extend({
+App.PinController = Ember.ObjectController.extend({
   isEditing: false,
-  edit: function() {
+  startEditing: function() {
     this.set('isEditing', true);
   },
   doneEditing: function (){
     this.set('isEditing', false);
-    this.get('store').commit();
   }
 });
 
-App.Post = DS.Model.extend({
-  title: DS.attr('string'),
-  author: DS.attr('string'),
-  intro: DS.attr('string'),
-  extended: DS.attr('string'),
-  publishedAt: DS.attr('date')
+
+App.Pin = DS.Model.extend({
+	pinnr: DS.attr('string'),
+	status: DS.attr('string'),
+	name: DS.attr('string'),
+	changedAt: DS.attr('date'),
 });
+
+App.Pin.FIXTURES =[{
+	id: 1,
+	pinnr: "1",
+    name: "TestPin1", 
+    status: "0", 
+    changedAt: new Date("2013-11-10")
+}, {
+	id: 2,
+	pinnr: "2",
+    name: "TestPin2", 
+    status: "0", 
+    changedAt: new Date("2013-11-09")
+}];
 
 Ember.Handlebars.registerBoundHelper('date', function(date) {
   return moment(date).fromNow();
-});
-
-var showdown = new Showdown.converter();
-Ember.Handlebars.registerBoundHelper('markdown', function(input) {
-  return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
 });
